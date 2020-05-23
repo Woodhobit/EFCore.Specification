3438f345c25b51ada490d5a9a6855530eaa9167d
Oleksii Hoshii <okeksii.hoshii@daxx.com>
2020-05-23 20:26:31 +0300

    Simple specification abstraction was added

---
+++ Specification/Contract/NotSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Specification.Contract
+{
+    public class NotSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> specification;
+
+        public NotSpecification(Specification<T> specification)
+        {
+            this.specification = specification;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            Expression<Func<T, bool>> spec = this.specification.ToExpression();
+
+            UnaryExpression notExpression = Expression.Not(spec);
+
+            return Expression.Lambda<Func<T, bool>>(
+                notExpression, spec.Parameters.Single());
+        }
+    }
+}
---
+++ Specification/Contract/OrSpecification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Specification.Contract
+{
+    public class OrSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> left;
+        private readonly Specification<T> right;
+
+        public OrSpecification(Specification<T> left, Specification<T> right)
+        {
+            this.right = right;
+            this.left = left;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            Expression<Func<T, bool>> leftExpression = this.left.ToExpression();
+            Expression<Func<T, bool>> rightExpression = this.right.ToExpression();
+
+            BinaryExpression orExpression = Expression.OrElse(
+                leftExpression.Body, rightExpression.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                orExpression, leftExpression.Parameters.Single());
+        }
+    }
+}
